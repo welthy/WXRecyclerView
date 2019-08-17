0c8492701cc508069229692957606b416ef4dcb3
welthy <381217445@qq.com>
2019-08-17 19:29:43 +0800

    update gradle version

--- app/src/main/java/wxrecyclerview/wx/cn/wxrecylerview/MainActivity.java
+++ app/src/main/java/wxrecyclerview/wx/cn/wxrecylerview/MainActivity.java
@@ -1,13 +1,38 @@
 package wxrecyclerview.wx.cn.wxrecylerview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button toWXRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void initView(){
+        toWXRecyclerView = findViewById(R.id.toWXRecycler);
+    }
+
+    private void initEvent(){
+        toWXRecyclerView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.toWXRecycler:
+                Intent wxRecyclerIt = new Intent(MainActivity.this,WXRecyclerViewDemo.class);
+                startActivity(wxRecyclerIt);
+                break;
+                default:
+                    break;
+        }
     }
 }
---
+++ app/src/main/java/wxrecyclerview/wx/cn/wxrecylerview/WXRecyclerViewDemo.java
@@ -0,0 +1,15 @@
+package wxrecyclerview.wx.cn.wxrecylerview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class WXRecyclerViewDemo extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.wx_recyclerview_demo);
+    }
+}
